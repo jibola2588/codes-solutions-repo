93848967a2d66938da55ca10f8bc48e6e2981abb
Ajibola Fawole <jibolafawole258@gmail.com>
2023-08-19 16:54:23 +0100

    added wheel events

---
+++ src/components/wheelEvent.vue
@@ -0,0 +1,166 @@
+<template>
+   <div class="wrapper">
+        <div>
+            <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nihil reiciendis aliquam hic. Nihil rerum repellat corporis nisi molestiae sequi neque, minus rem inventore consequatur facilis earum voluptatibus ipsa quidem laudantium!
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus nisi ex reiciendis illum delectus corrupti omnis quisquam commodi magnam eius eum est unde suscipit eos officiis magni quo, aut voluptates.
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fuga quod ipsum corrupti vitae ducimus, sit voluptatum illum nostrum itaque a aut qui non suscipit incidunt delectus, iusto veritatis. Est, distinctio.
+            </p>  
+            <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nihil reiciendis aliquam hic. Nihil rerum repellat corporis nisi molestiae sequi neque, minus rem inventore consequatur facilis earum voluptatibus ipsa quidem laudantium!
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus nisi ex reiciendis illum delectus corrupti omnis quisquam commodi magnam eius eum est unde suscipit eos officiis magni quo, aut voluptates.
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fuga quod ipsum corrupti vitae ducimus, sit voluptatum illum nostrum itaque a aut qui non suscipit incidunt delectus, iusto veritatis. Est, distinctio.
+            </p> 
+            <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nihil reiciendis aliquam hic. Nihil rerum repellat corporis nisi molestiae sequi neque, minus rem inventore consequatur facilis earum voluptatibus ipsa quidem laudantium!
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus nisi ex reiciendis illum delectus corrupti omnis quisquam commodi magnam eius eum est unde suscipit eos officiis magni quo, aut voluptates.
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fuga quod ipsum corrupti vitae ducimus, sit voluptatum illum nostrum itaque a aut qui non suscipit incidunt delectus, iusto veritatis. Est, distinctio.
+            </p> 
+            <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nihil reiciendis aliquam hic. Nihil rerum repellat corporis nisi molestiae sequi neque, minus rem inventore consequatur facilis earum voluptatibus ipsa quidem laudantium!
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus nisi ex reiciendis illum delectus corrupti omnis quisquam commodi magnam eius eum est unde suscipit eos officiis magni quo, aut voluptates.
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fuga quod ipsum corrupti vitae ducimus, sit voluptatum illum nostrum itaque a aut qui non suscipit incidunt delectus, iusto veritatis. Est, distinctio.
+            </p> 
+             <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nihil reiciendis aliquam hic. Nihil rerum repellat corporis nisi molestiae sequi neque, minus rem inventore consequatur facilis earum voluptatibus ipsa quidem laudantium!
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus nisi ex reiciendis illum delectus corrupti omnis quisquam commodi magnam eius eum est unde suscipit eos officiis magni quo, aut voluptates.
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fuga quod ipsum corrupti vitae ducimus, sit voluptatum illum nostrum itaque a aut qui non suscipit incidunt delectus, iusto veritatis. Est, distinctio.
+            </p>
+        </div>
+        <div class="scrollable">
+        <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nihil reiciendis aliquam hic. Nihil rerum repellat corporis nisi molestiae sequi neque, minus rem inventore consequatur facilis earum voluptatibus ipsa quidem laudantium!
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus nisi ex reiciendis illum delectus corrupti omnis quisquam commodi magnam eius eum est unde suscipit eos officiis magni quo, aut voluptates.
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fuga quod ipsum corrupti vitae ducimus, sit voluptatum illum nostrum itaque a aut qui non suscipit incidunt delectus, iusto veritatis. Est, distinctio.
+        </p>
+        <p>
+            Unde cumque modi voluptatibus fugit nemo harum, magni ea labore distinctio ullam officiis aliquid dolores enim nulla culpa saepe cum praesentium consequatur quaerat non officia perspiciatis? Repudiandae dignissimos architecto eius.
+            Amet totam esse reprehenderit corporis minus, nostrum, temporibus aut perferendis illum accusantium magni, quaerat odio consequuntur error ipsam reiciendis quia consequatur harum? Libero rerum dicta officia! Sequi soluta corrupti facere.
+            Maiores laudantium quisquam quaerat repellat voluptatibus doloremque quo nisi provident consequatur qui a quos, saepe laboriosam. Esse libero odit, aperiam, harum, officia quae accusamus quos voluptatibus distinctio sunt ut quasi.
+        </p>
+        <p>
+            Consectetur hic modi minima adipisci autem eum deleniti delectus dolorum. Nulla, officia numquam porro fugit laborum nesciunt itaque modi unde sunt nam odit quae vitae iure ipsa! Quas, ut quibusdam!
+            Eius voluptates molestiae a doloremque veniam? Quas doloribus ipsum unde ullam. Reprehenderit quas accusantium debitis suscipit id in culpa nobis? Inventore officiis fuga voluptate omnis velit. Illum molestiae ex sunt!
+            Corporis, dolor dolores? Expedita esse ratione odio temporibus unde quia ut nesciunt enim, iusto quidem natus excepturi magnam ex voluptas eveniet nostrum ducimus corrupti, velit, voluptatem distinctio ipsa. Corrupti, molestiae?
+        </p>
+        <p>
+            Ab, quo mollitia, ullam sit possimus saepe repellendus eius perferendis temporibus dignissimos minus distinctio. Totam, eum voluptate amet laudantium nam nisi omnis nobis animi et quas dicta commodi. Voluptate, culpa.
+            Ea eveniet consequuntur pariatur! Perferendis aliquid velit cum beatae commodi et voluptate nemo sit. Blanditiis minima distinctio a rerum praesentium quibusdam dolores animi culpa beatae nostrum, porro, tempora quis consectetur?
+            Illum provident sequi rem, amet cumque asperiores ad numquam veritatis eveniet, voluptas ab rerum modi error nam! Quaerat iste alias et error beatae possimus cum doloribus porro quod, sunt quia.
+        </p>
+    </div>
+    <div> 
+        <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nihil reiciendis aliquam hic. Nihil rerum repellat corporis nisi molestiae sequi neque, minus rem inventore consequatur facilis earum voluptatibus ipsa quidem laudantium!
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus nisi ex reiciendis illum delectus corrupti omnis quisquam commodi magnam eius eum est unde suscipit eos officiis magni quo, aut voluptates.
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fuga quod ipsum corrupti vitae ducimus, sit voluptatum illum nostrum itaque a aut qui non suscipit incidunt delectus, iusto veritatis. Est, distinctio.
+        </p>  
+         <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nihil reiciendis aliquam hic. Nihil rerum repellat corporis nisi molestiae sequi neque, minus rem inventore consequatur facilis earum voluptatibus ipsa quidem laudantium!
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus nisi ex reiciendis illum delectus corrupti omnis quisquam commodi magnam eius eum est unde suscipit eos officiis magni quo, aut voluptates.
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fuga quod ipsum corrupti vitae ducimus, sit voluptatum illum nostrum itaque a aut qui non suscipit incidunt delectus, iusto veritatis. Est, distinctio.
+        </p>
+         <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nihil reiciendis aliquam hic. Nihil rerum repellat corporis nisi molestiae sequi neque, minus rem inventore consequatur facilis earum voluptatibus ipsa quidem laudantium!
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus nisi ex reiciendis illum delectus corrupti omnis quisquam commodi magnam eius eum est unde suscipit eos officiis magni quo, aut voluptates.
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fuga quod ipsum corrupti vitae ducimus, sit voluptatum illum nostrum itaque a aut qui non suscipit incidunt delectus, iusto veritatis. Est, distinctio.
+        </p>
+         <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nihil reiciendis aliquam hic. Nihil rerum repellat corporis nisi molestiae sequi neque, minus rem inventore consequatur facilis earum voluptatibus ipsa quidem laudantium!
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus nisi ex reiciendis illum delectus corrupti omnis quisquam commodi magnam eius eum est unde suscipit eos officiis magni quo, aut voluptates.
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fuga quod ipsum corrupti vitae ducimus, sit voluptatum illum nostrum itaque a aut qui non suscipit incidunt delectus, iusto veritatis. Est, distinctio.
+        </p>
+         <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nihil reiciendis aliquam hic. Nihil rerum repellat corporis nisi molestiae sequi neque, minus rem inventore consequatur facilis earum voluptatibus ipsa quidem laudantium!
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus nisi ex reiciendis illum delectus corrupti omnis quisquam commodi magnam eius eum est unde suscipit eos officiis magni quo, aut voluptates.
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fuga quod ipsum corrupti vitae ducimus, sit voluptatum illum nostrum itaque a aut qui non suscipit incidunt delectus, iusto veritatis. Est, distinctio.
+        </p>
+    </div>
+</div>
+</template>
+
+<script>
+//     const scrollable = document.querySelector('.scrollable');
+// const scrollStep = 50;  // Number of pixels to scroll per step
+
+// scrollable.addEventListener('wheel', function(e) {
+//     e.preventDefault();  // Prevent default scroll behavior
+
+//     const scrollLeft = this.scrollLeft;
+//     const scrollWidth = this.scrollWidth - this.clientWidth;
+//     const delta = Math.sign(e.deltaY) * scrollStep;
+
+//     const newScrollLeft = Math.max(0, Math.min(scrollLeft + delta, scrollWidth));
+//     this.scrollLeft = newScrollLeft;
+// }, { passive: false });
+
+
+
+// const scrollable = document.querySelector('.scrollable');
+// const scrollStep = 50;  // Number of pixels to scroll per step
+
+// scrollable.addEventListener('wheel', function(e) {
+//     const scrollLeft = this.scrollLeft;
+//     const scrollWidth = this.scrollWidth - this.clientWidth;
+//     const delta = Math.sign(e.deltaY) * scrollStep;
+
+//     const newScrollLeft = Math.max(0, Math.min(scrollLeft + delta, scrollWidth));
+//     this.scrollLeft = newScrollLeft;
+
+//     console.log('scroll left is ',newScrollLeft)
+
+//     // If at the beginning or end of content, allow default scroll behavior
+//     if (newScrollLeft === 0 || newScrollLeft === scrollWidth) {
+//         e.preventDefault();
+//     }
+// }, { passive: false });
+
+
+const scrollable = document.querySelector('.scrollable');
+const scrollStep = 50;  // Number of pixels to scroll per step
+
+scrollable.addEventListener('wheel', function(e) {
+    const scrollLeft = this.scrollLeft;
+    const scrollWidth = this.scrollWidth - this.clientWidth;
+    const delta = Math.sign(e.deltaY) * scrollStep;
+
+    const newScrollLeft = Math.max(0, Math.min(scrollLeft + delta, scrollWidth));
+    
+    // Allow default scroll behavior if at the beginning or end of content
+    if (newScrollLeft === 0 || newScrollLeft === scrollWidth) {
+        e.stopPropagation();  // Stop propagation to parent elements
+        return;
+    }
+
+    e.preventDefault();  // Prevent default scroll behavior
+    this.scrollLeft = newScrollLeft;
+}, { passive: false });
+
+
+
+</script>
+
+<style>
+.wrapper{ 
+    width: 520px;
+    padding: 22px;
+    border-radius: 8px;
+    background-color: #fff;
+    box-shadow:  0px 5px 10px rgba(0,0,0,0.4);
+   }
+
+   .scrollable{ 
+    white-space: nowrap;
+    padding: 8px 0px;
+    overflow-y: hidden;
+   }
+   ::-webkit-scrollbar{
+    height: 6px;
+   }
+   ::-webkit-scrollbar-thumb{
+    background-color: #ccc;
+   }
+   ::-webkit-scrollbar-track{
+    background-color: azure;
+   }
+</style>
